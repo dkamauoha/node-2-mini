dca2befc818717d34a1d93fbc5f8040f81b6eb84
Dylan Kamauoha <dylantkamauoha@gmail.com>
2018-07-11 15:18:19 -0600

    Finished Mini

---
+++ controller.js
@@ -0,0 +1,14 @@
+
+
+modules.exports = {
+    getPlanes: function (req, res, next) {
+        req.app.get('db')
+
+        dbInstance.get_planes([25])
+            .then(planes => res.status(200).send(planes))
+            .catch(err => {
+                console.log(err);
+                res.status(500).send(err);
+            })
+    }
+}
